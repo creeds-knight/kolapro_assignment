778181064db2c2af5cc7c860b4f85469ac95d73f
creeds-knight <apedoarthur21@gmail.com>
2024-11-25 23:20:29 +0300

    Adding mail send logic

--- KOLAPRO_ASSIGMENT/custom_purchase/models/custom_purchase_order.py
+++ KOLAPRO_ASSIGMENT/custom_purchase/models/custom_purchase_order.py
@@ -10,10 +10,22 @@ class CustomPurchaseOrder(models.Model):
                                    domain=[('supplier_rank', '>', 0)])
 
     def action_rfq_send(self):
+        if not self.vendor_ids:
+            return super(CustomPurchaseOrder, self).action_rfq_send()
+
+        template_id = self.env.ref('purchase.email_template_edi_purchase').id
+        mail_template = self.env['mail.template'].browse(template_id)
+
         for vendor in self.vendor_ids:
             rfq_copy = self.copy(default={
                 'partner_id': vendor.id,
                 'state': 'draft',
             })
             rfq_copy.button_confirm()
+            mail_template.with_context(email_to=vendor.email).send_mail(rfq_copy.id, force_send=True)
+
+        res = super(CustomPurchaseOrder, self).action_rfq_send()
+        if 'context' in res:
+            res['context']['vendor_ids'] = [v.id for v in self.vendor_ids]
+        return res
 
